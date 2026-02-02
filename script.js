f62c4c4cdde06127de69a0119dfc9a7f04a016ad
Chris2-del305 <Chrisvoice37@gmail.com>
2026-02-02 21:43:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition;
+let started = false;
 
-const recognition = new SpeechRecognition();
-recognition.lang = "en-US";
-recognition.continuous = true;
+function startMic() {
+  if (started) return;
+  started = true;
 
-recognition.start();
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
 
-recognition.onresult = function (event) {
-  const spoken =
-    event.results[event.results.length - 1][0].transcript;
+  recognition = new SpeechRecognition();
+  recognition.lang = "en-US";
+  recognition.continuous = true;
 
-  alert("You said: " + spoken);
-};
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    const spoken =
+      event.results[event.results.length - 1][0].transcript;
+
+    alert("You said: " + spoken);
+  };
+}
